f7d8617085b3641aeb9ad6f2cda3eb739d6d479c
julioau15 <julioaugustov@gmail.com>
2025-11-19 21:14:42 -0300

    Adiconado opção de gerar relatorio em txt e Json

--- src/MediaAlunos.py
+++ src/MediaAlunos.py
@@ -1,3 +1,5 @@
+import json
+
 def receberDados():
     quantidadeAlunos = int(input("Quantos alunos tem na classe? "))
 
@@ -37,14 +39,14 @@ def exibirResultado(nomesAlunos, nota1, nota2, medias, mediaTurma, situacao):
         print(f"Nome: {nomesAlunos[i]}")
         print(f"Nota1: {nota1[i]}")
         print(f"Nota2: {nota2[i]}")
-        print(f"Média: {medias[i]}")
+        print(f"Média: {medias[i]:2f}")
         print(f"situação: {situacao[i]}")
         print("\n")
 
     print("------------------------------")
     print("** Relatório geral da turma **")
     print("------------------------------")
-    print(f"Média geral da turma: {mediaTurma}")
+    print(f"Média geral da turma: {mediaTurma:2f}")
     print(f"maior media: {max(medias)}")
     print(f"Menor media: {min(medias)}")
     print(f"Aprovados: {situacao.count('Aprovado!')}")
@@ -52,12 +54,86 @@ def exibirResultado(nomesAlunos, nota1, nota2, medias, mediaTurma, situacao):
     print(f"Reprovados: {situacao.count('Reprovado!') }")
     print("------------------------------")
 
+
+def salvarTxt(nomesAlunos, nota1, nota2, medias, mediaTurma, situacao):
+    with open("relatorio.txt","w") as arquivo:
+        arquivo.write("RELATÓRIO FINAL\n")
+        arquivo.write("----------------------\n")
+
+        arquivo.write(
+            "------------------------------"
+            "** Relatório geral da turma **"
+            "------------------------------"
+            f"Média geral da turma: {mediaTurma:2f} |"
+            f"maior media: {max(medias)} |"
+            f"Menor media: {min(medias)} |"
+            f"Aprovados: {situacao.count('Aprovado!')} |"
+            f"Recuperações: {situacao.count('Recuperação!')} |"
+            f"Reprovados: {situacao.count('Reprovado!') } \n|"
+        )
+
+        for i in range(len(nomesAlunos)):
+            arquivo.write(
+                f"Nome: {nomesAlunos[i]} |"
+                f"Nota 1: {nota1[i]} |"
+                f"Nota 2: {nota2[i]} |"
+                f"Média: {medias[i]} |"
+                f"Situação: {situacao[i]} |\n"
+            )
+
+    print("Arquivo relatorio.txt gerado com sucesso!")
+
+def salvarJson(nomesAlunos, nota1, nota2, medias, mediaTurma, situacao):
+    dados = []
+
+    for i in range(len(nomesAlunos)):
+        dados.append({
+            "Nome": nomesAlunos[i],
+            "Nota 1": nota1[i],
+            "Nota 2": nota2[i],
+            "Média": medias[i],
+            "Situação": situacao[i]
+        })
+
+    relatorioGeral = {
+        "quantidadeAlunos": len(nomesAlunos),
+        "Média geral da turma": mediaTurma,
+        "maior media": max(medias),
+        "Menor media": min(medias),
+        "Aprovados": situacao.count('Aprovado'),
+        "Recuperações": situacao.count('Recuperação!'),
+        "Reprovados": situacao.count('Reprovado!'),
+        "alunos": dados
+
+    }
+
+    with open('relatorio.json', 'w') as arquivo:
+        json.dump(relatorioGeral, arquivo, indent = 4, ensure_ascii=False)
+
+    print("Arquivo relatorio.txt gerado com sucesso!")
+
+def escolherFormato(nomesAlunos, nota1, nota2, medias, mediaTurma, situacao):
+    print("\n Deseja salvar o relatorio como:")
+    print("1 - TXT")
+    print("2 - JSON")
+    print("3 - Não salvar")
+    opcao = input("Escolha: ")
+
+    if opcao == "1":
+        salvarTxt(nomesAlunos, nota1, nota2, medias, mediaTurma, situacao)
+    elif opcao == "2":
+        salvarJson(nomesAlunos, nota1, nota2, medias, mediaTurma, situacao)
+    else:
+        print("Finalizado!")
+
+
 def main():
     quantidadeAlunos, nomesAlunos, nota1, nota2 = receberDados()
     medias = calcularMediaAluno(nota1, nota2)
     mediaTurma = calcularMediaTurma(medias)
     situacao = definirSituacao(medias)
     exibirResultado(nomesAlunos, nota1, nota2, medias, mediaTurma, situacao)
+    escolherFormato(nomesAlunos, nota1, nota2, medias, mediaTurma, situacao)
 
 
 main()   
